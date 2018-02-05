2ae8eda29144b1e1dd1dc82e429ce1070ecd54a6
Kenan-plugdj <marlons.r.davila@gmail.com>
2018-02-05 21:51:23 -0200

    Update CMBot.js

--- CMBot.js
+++ CMBot.js
@@ -2964,44 +2964,47 @@
                 }
             },         
          
-            cantadasCommand: {
-                command: 'ct',
+             cantadaCommand: {
+                command: ['ct','cantada','conquistar'],
                 rank: 'user',
-                type: 'startsWith',
-                getCantadases: function (chat) {
-                    var sho = Math.floor(Math.random() * basicBot.chat.cantadases.length);
-                    return basicBot.chat.cantadases[sho];
+                type: 'exact',
+                getCantada: function(chat) {
+                    var c = Math.floor(Math.random() * basicBot.chat.cantada.length);
+                    return basicBot.chat.cantada[c];
                 },
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                functionality: function(chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void(0);
                     else {
                         var msg = chat.message;
+
                         var space = msg.indexOf(' ');
                         if (space === -1) {
-                            API.sendChat(basicBot.chat.eatcantadas);
+                            API.sendChat(basicBot.chat.botCantada);
                             return false;
-                        }
-                        else {
+                        } else {
                             var name = msg.substring(space + 2);
                             var user = basicBot.userUtilities.lookupUserName(name);
-                            if (name === "djs") {
-                                return API.sendChat(subChat(basicBot.chat.multicantadas, {namefrom: chat.un, weed: this.getCantadases()}));
-                            }
-                            else if (user === false || !user.inRoom) {
-                                return API.sendChat(subChat(basicBot.chat.nousercantadas, {name: name}));
-                            }
-                            else if (user.username === chat.un) {
-                                return API.sendChat(subChat(basicBot.chat.selfcantadas, {name: name}));
-                            }
-                            else {
-                                return API.sendChat(subChat(basicBot.chat.cantadas, {nameto: user.username, namefrom: chat.un, cantadas: this.getCantadases()}));
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nouserCantada, {
+                                    name: name
+                                }));
+                            } else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfCantada, {
+                                    name: name
+                                }));
+                            } else {
+                                return API.sendChat(subChat(basicBot.chat.cantadas, {
+                                    nameto: user.username,
+                                    namefrom: chat.un,
+                                    cantada: this.getCantadas()
+                                }));
                             }
                         }
                     }
                 }
-            },             
-
+            },
+         
             languageCommand: {
                 command: 'language',
                 rank: 'manager',
