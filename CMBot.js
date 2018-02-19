33b8a5f98e51fac98601671201f6ded874b2a6ae
Kenan-plugdj <marlons.r.davila@gmail.com>
2018-02-18 22:20:15 -0300

    Update CMBot.js

--- CMBot.js
+++ CMBot.js
@@ -325,7 +325,7 @@
             rulesLink: null,
             themeLink: null,
             fbLink: null,
-            discordLink: 'https://discord.gg/c66sn3n',
+            discordLink: 'https://discord.gg/xVEZ3ZK',
             emotesLink: null,
             youtubeLink: null,
             website: null,
